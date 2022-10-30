a6b77c0c9224dae40069a80ce85d06d1b9675307
vinielelopes <vinielelopes@outlook.com>
2022-10-30 15:48:40 -0200

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/PolosResilia">
+    <BrowserRouter>
     <App>
       <Polo1/>
       <Polo2/>
