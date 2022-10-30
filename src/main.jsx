22ce6478847872387eb36b7eec4be78d7544d271
vinielelopes <vinielelopes@outlook.com>
2022-10-30 02:04:53 -0200

    fix: refresh page

--- src/main.jsx
+++ src/main.jsx
@@ -4,18 +4,16 @@ import App from './App'
 import {Polo1} from './components/Polos/Polo1';
 import {Polo2} from './components/Polos/Polo2';
 import './index.css'
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
     <HashRouter>
     <App>
       <Polo1/>
       <Polo2/>
     </App>
     </HashRouter>
-    </BrowserRouter>
   </React.StrictMode>
 )
