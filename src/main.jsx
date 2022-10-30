6e7e203a661b4401a5fc1e3fdd53c2d29f9b70bc
vinielelopes <vinielelopes@outlook.com>
2022-10-30 02:13:51 -0200

    fix: routes

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,16 @@ import App from './App'
 import {Polo1} from './components/Polos/Polo1';
 import {Polo2} from './components/Polos/Polo2';
 import './index.css'
-import {HashRouter} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
     <App>
       <Polo1/>
       <Polo2/>
     </App>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 )
