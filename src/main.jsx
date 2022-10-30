0dc3a940c869fed8a3513c01d6774fd9ae535f6f
vinielelopes <vinielelopes@outlook.com>
2022-10-30 15:21:02 -0200

    fix: refresh page

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/PolosResilia">
     <App>
       <Polo1/>
       <Polo2/>
